bf888fb73895487ab904a469608b35e6b003268a
Chirag-Sharma-08 <257071564+ChiragSharma-08@users.noreply.github.com>
2026-06-10 22:06:56 +0530

    2.1.Series in python
    Intro

---
+++ 2.series.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+#Series = A Pandas 1-Dimensional labeled array that can hold any data type
+#         Think of it like a single column in a spreadsheet(1-Dimentional)
+
+data = [100, 103,  104]
+
+series = pd.Series(data)
+
+print(series)
