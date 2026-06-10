41d19175d6670b655278f91ef85fd935300975b5
Chirag-Sharma-08 <257071564+ChiragSharma-08@users.noreply.github.com>
2026-06-10 22:24:36 +0530

    2.5.conditional sentence in series

--- 2.series.py
+++ 2.series.py
@@ -3,12 +3,9 @@ import pandas as pd
 #Series = A Pandas 1-Dimensional labeled array that can hold any data type
 #         Think of it like a single column in a spreadsheet(1-Dimentional)
 
-data = [100, 103,  104]
+data = [100, 103,  104, 203, 304, 250]
 
-series = pd.Series(data, index = ["a", "b", "c"])
+series = pd.Series(data, index = ["a", "b", "c", "d", "e", "f"])
 
-print(series.iloc[0])
+print(series[series<200])
 
-series.loc["a"] = 200
-
-print(series.iloc[0])
